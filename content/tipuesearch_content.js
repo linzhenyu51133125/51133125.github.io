a06478b26f1805e386a03f1ec7d4d22cf4692d54
51133125 <linmuke123@gmail.com>
2023-05-26 09:09:45 +0800

    0

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://github.com/linzhenyu51133125/51133125.github.io \xa0 \n https://51133125.github.io \xa0 \n', 'tags': '', 'url': 'About.html'}, {'title': 'STL', 'text': '\n', 'tags': '', 'url': 'STL.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://github.com/linzhenyu51133125/51133125.github.io \xa0 \n https://linzhenyu51133125.github.io/51133125.github.io/content/index.html   \n', 'tags': '', 'url': 'About.html'}, {'title': 'STL', 'text': '\n', 'tags': '', 'url': 'STL.html'}]};
